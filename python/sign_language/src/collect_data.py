8b9ff0d6470b2beb94359f5393882c705592160b
MarcRGM <marcrodniemanalo@gmail.com>
2025-12-09 10:46:07 +0800

    Setup hand detection object and web cam

--- python/sign_language/src/collect_data.py
+++ python/sign_language/src/collect_data.py
@@ -4,3 +4,17 @@ import mediapipe as mp # Detects hand and gives it Landmark
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
+# Hand detection
+hands = mp_hands.Hands (
+    static_image_mode=False # Track hands across frames
+    max_num_hands=2 # detects 1 hand
+    min_detection_confidence=0.5 # 50% hand detection
+    min_tracking_confidence=0.5 # 50% tracking between frames
+)
+
+# Opens the webcam
+cap = cv2.VideoCapture(0) # open camera index 0 (Main camera)
+# cap read frames from camera
+
+
+
