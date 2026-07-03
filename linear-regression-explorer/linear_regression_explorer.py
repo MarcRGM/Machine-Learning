566abb9e52c0abe9f50bdeb4ea2c4b271a554029
MarcRGM <marcrodniemanalo@gmail.com>
2026-07-03 10:54:16 +0800

    feat: reshape house size to 2D for scikit-learn

--- linear-regression-explorer/linear_regression_explorer.py
+++ linear-regression-explorer/linear_regression_explorer.py
@@ -5,6 +5,15 @@ from sklearn.model_selection import train_test_split # For training
 
 model = LinearRegression()
 
-house_sizes = np.random.uniform(30.0, 200.0, 50) # low (inclusive), high (exclusive), count
+# features = column
+# samples = row
+
+house_sizes = np.random.uniform(30.0, 200.0, 50).reshape(-1, 1) 
+# np.random.uniform() - low (inclusive), high (exclusive), count
+# .reshape(-1, 1) - scikit‑learn requires features to be 2D
+# -1 to automatically adjust based on how many values the array contains
 noise = np.random.normal(0, 20000, 50) # mean, std, count
 price = house_sizes * 1500 + 20000 + noise
+
+
+
